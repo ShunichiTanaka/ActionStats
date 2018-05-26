3fcef4758abfae117a7fb4c0690f71114911637f
Street Lethal <street.lethal.57@gmail.com>
2018-05-26 16:44:55 +0900

    refs #1 公開中の「何した」を削除禁止にする 公開中の「何した」が含まれるカテゴリも削除禁止にする

--- server/app/models/category.rb
+++ server/app/models/category.rb
@@ -1,5 +1,11 @@
 class Category < ApplicationRecord
   has_many :outcomes, dependent: :destroy
+  has_many :published_outcomes, -> { published }, class_name: 'Outcome'
 
   validates :name, presence: true, uniqueness: true
+
+  # そのカテゴリに公開中の「何した」が含まれるか
+  def has_published_outcomes?
+    published_outcomes.limit(1).present?
+  end
 end
--- server/app/models/outcome.rb
+++ server/app/models/outcome.rb
@@ -2,4 +2,6 @@ class Outcome < ApplicationRecord
   belongs_to :category
 
   validates :name, presence: true, uniqueness: true
+
+  scope :published, -> { where published: true }
 end
