61fea863ab93aeb8ea7c72b077c00b23e8b992bc
root <root@localhost.localdomain>
2018-05-31 12:49:06 +0900

    modelの概要を表示

--- server/app/models/administrator.rb
+++ server/app/models/administrator.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: administrators
+#
+#  id                     :bigint(8)        not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  failed_attempts        :integer          default(0), not null
+#  unlock_token           :string
+#  locked_at              :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
 class Administrator < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- server/app/models/outcome.rb
+++ server/app/models/outcome.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: outcomes
+#
+#  id            :bigint(8)        not null, primary key
+#  category_id   :integer          not null
+#  name          :string           not null
+#  published     :boolean          default(FALSE), not null
+#  display_order :integer          default(0), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Outcome < ApplicationRecord
   belongs_to :category
 
