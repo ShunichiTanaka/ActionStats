64f959089b25796feb7e91f0019537722f298d81
Street Lethal <street.lethal.57@gmail.com>
2018-06-01 01:41:33 +0900

    管理画面よりデータ確認 SQL発行抑制

--- server/app/controllers/service/users_outcomes_controller.rb
+++ server/app/controllers/service/users_outcomes_controller.rb
@@ -9,22 +9,24 @@ module Service
     private
 
     def load_users_outcomes
-      @q = UsersOutcome.search(params[:q])
-      @users_outcomes = @q.result
-                          .select('users_outcomes.outcome_id, users_outcomes.reaction, COUNT(*) AS cnt')
-                          .group(:outcome_id, :reaction)
-                          .order(:outcome_id, :reaction)
+      @q = UsersOutcome.includes(:outcome).search(params[:q])
+      users_outcomes = @q.result
+                         .select('users_outcomes.outcome_id, users_outcomes.reaction, COUNT(*) AS cnt')
+                         .group(:outcome_id, :reaction)
+                         .order(:outcome_id, :reaction)
+                         .to_a
+      @users_outcomes = users_outcomes.group_by(&:outcome_id)
     end
 
     def load_outcomes
-      @all_outcome_names = Outcome.all
-      @selected_outcomes = @q.outcome_id_in.present? ? Outcome.where(id: @q.outcome_id_in) : @all_outcome_names
+      @all_outcome_names = Outcome.select(:id, :name)
     end
 
     def load_comments
       all_comments = @q.result
       all_comments = all_comments.where.not(comment: nil) if params[:show_no_comment].present?
-      @comments = all_comments.includes(:user).order(id: :desc).limit(1000)
+      comments = all_comments.includes(:user).order(id: :desc).limit(1000).to_a
+      @comments = comments.group_by(&:outcome_id)
     end
   end
 end
