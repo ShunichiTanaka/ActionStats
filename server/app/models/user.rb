72b20f5f05f95a4ea4cc53ea69e5a5222750cb77
Street Lethal <street.lethal.57@gmail.com>
2018-05-31 22:52:06 +0900

    管理画面よりデータ確認 テストデータを本物らしく、%表示を少数第一位まで表示

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -13,10 +13,8 @@ class User < ApplicationRecord
   before_create :set_registered_at
 
   def age
-    [
-      Time.current.year - year_of_birth.to_i - 1, # 最も若くなるように計算
-      0
-    ].max
+    # 最も若くなるように計算
+    [Time.current.year - year_of_birth.to_i - 1, 0].max
   end
 
   private
--- server/db/seeds/users_outcomes.rb
+++ server/db/seeds/users_outcomes.rb
@@ -10,10 +10,10 @@ module Seeds
         9.downto(0) do |i| # 10日分
           post_date = i.days.ago.to_date
           puts "  #{post_date} 分作成"
-          24.times do |j|
-            rand(20).times do # 1時間に最大20件投稿
+          24.times do |post_hour|
+            rand(20).times do |post_min| # 1時間に最大20件投稿
               users_outcome = UsersOutcome.new
-              users_outcome_params(users_outcome, post_date, j)
+              users_outcome_params(users_outcome, post_date, post_hour, post_min)
               users_outcome.save
             end
           end
@@ -23,14 +23,15 @@ module Seeds
 
       private
 
-      def users_outcome_params(users_outcome, post_date, post_time)
+      def users_outcome_params(users_outcome, post_date, post_hour, post_min)
         users_outcome.post_date = post_date
-        users_outcome.post_time = post_time
+        users_outcome.post_time = post_hour
         users_outcome.user_id = rand(1000) + 1
         users_outcome.outcome_id = rand(29) + 1
         reaction = rand(4) + 1
         users_outcome.reaction = reaction
         comment = reaction >= 3 ? GOOD_COMMENTS.sample : BAD_COMMENTS.sample
+        users_outcome.created_at = post_date.in_time_zone + post_hour.hours + post_min.minutes
         return if rand <= 0.5 # 1/2 の確率でノーコメント
         users_outcome.comment = comment
       end
